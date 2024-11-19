5a0a000ede072eb7603bb8c6fff27abf69148a16
Ayub <ayubmja@icloud.com>
2024-11-19 13:39:48 -0500

    first init

---
+++ data/index.js
@@ -0,0 +1,22 @@
+export const champions = [
+    {
+        id:1,
+        name: "Hasan Piker",
+        age: "33",
+        height: "1.91m",
+        onlineName: "Hasanabi",
+        img: "test.jp",
+        info: "Talks about politics and stuff decent at basketball",
+        enemies: "Destiny"
+    },
+    {
+        id:2,
+        name: "Steven Kenneth Bonnell II",
+        onlineName: "Destiney",
+        age: "35",
+        height: "1.73m",
+        img: "test.jp",
+        info: "Talks about politics and stuff better at basketball than Hasan dunks on him regularly",
+        enemies: "Hasan Piker"
+    }
+]
